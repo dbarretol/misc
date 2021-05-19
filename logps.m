30956ee73431e68058d70c145e68e9ea94fdda27
dbarretol <57958754+dbarretol@users.noreply.github.com>
2021-05-19 17:59:04 -0500

    Update logps.m

--- logps.m
+++ logps.m
@@ -45,22 +45,6 @@ function [T, pvalue, Tn,H]=logps(phat,data,varargin)
 %
 % See also loglike, proflog
 
-
-% Copyright (C) 2000 Per A. Brodtkorb
-%
-% This program is free software; you can redistribute it and/or modify
-%     it under the terms of the GNU Lesser General Public License as published by
-%     the Free Software Foundation; either version 3 of the License, or
-%     (at your option) any later version.
-% 
-%     This program is distributed in the hope that it will be useful,
-%     but WITHOUT ANY WARRANTY; without even the implied warranty of
-%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%     GNU General Public License for more details.
-% 
-%     You should have received a copy of the GNU Lesser General Public License
-%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 % Reference
 %
 % R. C. H. Cheng; N. A. K. Amin (1983)
