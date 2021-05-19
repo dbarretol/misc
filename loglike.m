16f74e015ee0744a6e4e48c4ba0f1a8fdce9fb18
dbarretol <57958754+dbarretol@users.noreply.github.com>
2021-05-19 18:02:24 -0500

    Update loglike.m

--- loglike.m
+++ loglike.m
@@ -33,23 +33,6 @@ function [LL,pcov,H]=loglike(phat,data,varargin)
 % See also logps, proflog
 
 
-% Copyright (C) 2000 Per A. Brodtkorb
-%
-% This program is free software; you can redistribute it and/or modify
-%     it under the terms of the GNU Lesser General Public License as published by
-%     the Free Software Foundation; either version 3 of the License, or
-%     (at your option) any later version.
-% 
-%     This program is distributed in the hope that it will be useful,
-%     but WITHOUT ANY WARRANTY; without even the implied warranty of
-%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%     GNU General Public License for more details.
-% 
-%     You should have received a copy of the GNU Lesser General Public License
-%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
 %Tested on: matlab 5.3
 % History:
 % revised pab May 2007
